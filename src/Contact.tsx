5e0a22f2b2d2348431043d2fd458c78df24fd6d1
Meredith Fronk <merebrownie@me.com>
2020-07-15 17:25:13 -0400

    typescript contact section

--- src/Contact.tsx
+++ src/Contact.tsx
@@ -1,23 +1,24 @@
-import React, { useState, createRef } from 'react';
-import { Button, FormControl, Input, Textarea, Box, useDisclosure } from '@chakra-ui/core';
+import React, { FC, ReactElement, useState, createRef } from 'react';
+import { FormControl, Input, Textarea, Box, useDisclosure } from '@chakra-ui/core';
 import ReCAPTCHA from 'react-google-recaptcha';
 import emailjs from 'emailjs-com';
 import Modal from './Modal';
 import SectionHeading from './components/SectionHeading';
+import Button from './components/Button';
 
-const Contact = () => {
+const Contact: FC = (): ReactElement => {
 	const { isOpen, onOpen, onClose } = useDisclosure();
-	const [loading, setLoading] = useState();
+	const [loading, setLoading] = useState(false);
 	const [error, setError] = useState(true);
 
-	const recaptchaRef = createRef();
+	const recaptchaRef = createRef<ReCAPTCHA>();
 
-	const sendEmail = (e) => {
+	const sendEmail = (e: React.FormEvent<HTMLFormElement>) => {
 		setError(false);
 		setLoading(true);
 		e.preventDefault();
 
-		return emailjs.sendForm('icloud', 'template_KRhM5nMN', e.target, 'user_KQXR5OSjppGBpBtgTf4Mb').then(
+		return emailjs.sendForm('icloud', 'template_KRhM5nMN', e.target as any, 'user_KQXR5OSjppGBpBtgTf4Mb').then(
 			(result) => {
 				setLoading(false);
 			},
@@ -71,17 +72,9 @@ const Contact = () => {
 						/>
 					</FormControl>
 					<Box ml="auto" mr="auto" width="304px" mb="3">
-						<ReCAPTCHA
-							style={{ width: '304px' }}
-							ml="auto"
-							mr="auto"
-							ref={recaptchaRef}
-							sitekey="6LcS_7AZAAAAAI-RxystXhg2DbLsZ47Ze58vc8on"
-						/>
+						<ReCAPTCHA ref={recaptchaRef} sitekey="6LcS_7AZAAAAAI-RxystXhg2DbLsZ47Ze58vc8on" />
 					</Box>
-					<Button type="submit" variant="outline" onClick={onOpen}>
-						Send
-					</Button>
+					<Button text="Send" type="submit" onClick={onOpen} isLoading={loading} />
 				</form>
 			</Box>
 
