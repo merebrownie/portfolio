86116aa4d17dba35ba9f09091d4a833614f53994
Meredith Fronk <merebrownie@me.com>
2020-07-18 15:35:14 -0400

    use ReactGA

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ReactGA from 'react-ga';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+ReactGA.initialize('UA-89191904-1', { debug: true });
+ReactGA.pageview(window.location.pathname + window.location.search);
 
 ReactDOM.render(
 	<React.StrictMode>
